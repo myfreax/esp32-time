8de4ba70e24849b4f0feb0ff6077f8d9ddc21d47
myfreax <web@myfreax.com>
2024-01-12 11:40:53 +0800

    feat: added timing callback function

--- include/time.h
+++ include/time.h
@@ -1,5 +1,11 @@
 #pragma once
+#include <stdbool.h>
 #include <stdint.h>
 #include <sys/time.h>
 
+typedef void (*time_delay_callback_t)(void* arg);
+
 int64_t time_currnet_us();
+void time_delay_callback(char* name, int64_t* start, bool start_condition,
+                         bool cancel_condition, int64_t end,
+                         time_delay_callback_t callback, void* arg);
--- time.c
+++ time.c
@@ -1,8 +1,31 @@
 #include "time.h"
 
+#include "esp_log.h"
+static const char* TAG = "TIME";
+
 int64_t time_currnet_us() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   int64_t time_us = (int64_t)tv.tv_sec * 1000000L + (int64_t)tv.tv_usec;
   return time_us;
+}
+
+void time_delay_callback(char* name, int64_t* start, bool start_condition,
+                         bool cancel_condition, int64_t end,
+                         time_delay_callback_t callback, void* arg) {
+  if (start_condition && *start == 0) {
+    ESP_LOGI(TAG, "Start %s Timing", name);
+    *start = time_currnet_us();
+  }
+
+  if (start_condition && (time_currnet_us() - *start) > end) {
+    ESP_LOGI(TAG, "%s Timing End", name);
+    callback(arg);
+    *start = 0;
+  }
+
+  if (cancel_condition && *start != 0) {
+    ESP_LOGI(TAG, "Cancel %s Timing", name);
+    *start = 0;
+  }
 }
